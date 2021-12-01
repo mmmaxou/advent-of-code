8eca46b05513480bafe536364a75b645861d3037
amalia.pluchard <amalia.pluchard@teamto.com>
2021-12-01 19:30:37 +0100

    day 1

---
+++ 2021/day-1/answer.py
@@ -0,0 +1,21 @@
+import os
+
+inputPath = os.path.join(os.path.dirname(__file__), "input")
+
+with open(inputPath, "r") as inputFile:
+    lines = [int(line) for line in inputFile.readlines() if line]
+
+
+def answer(iterable, slidingWindow):
+    count = 0
+    for i in range(len(iterable) - slidingWindow + 1):
+        if i > 0:
+            current = sum(iterable[i: i + slidingWindow])
+            before = sum(iterable[i - 1: i - 1 + slidingWindow])
+            if current > before:
+                count += 1
+    return count
+
+
+print(answer(lines, 1))
+print(answer(lines, 3))
