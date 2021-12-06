e53be1fc17045fd468ab83c347badd2f431aaf52
amalia.pluchard <amalia.pluchard@teamto.com>
2021-12-06 20:24:44 +0100

    day 6 (smol)

--- 2021/day-6/answer.py
+++ 2021/day-6/answer.py
@@ -1,83 +1,21 @@
 import os
-from collections import defaultdict
+from collections import Counter
 
-inputPath = os.path.join(os.path.dirname(__file__), "input")
-
-with open(inputPath, "r") as inputFile:
+with open(os.path.join(os.path.dirname(__file__), "input"), "r") as inputFile:
     lines = [list(map(int, line.split(","))) for line in inputFile.readlines()][0]
 
-CHILDBIRTH_CYCLE = 7
-CHILDBIRTH_BIRTH_DELAY = 2
 
-
-class Sea(object):
-    def __init__(self, fishes):
-        self.generations = []
-        self.fishesByChildbirthDaysLeft = defaultdict(int)
-        for fish in fishes:
-            self.fishesByChildbirthDaysLeft[fish] += 1
-        self.generations.append(self.fishesByChildbirthDaysLeft)
-
-    def nextDay(self):
-        newDay = defaultdict(int)
-        for childBirthDayLeft, amount in self.fishesByChildbirthDaysLeft.items():
-            newDay[childBirthDayLeft - 1] = amount
-        eggs = newDay[-1]
-        newDay[CHILDBIRTH_CYCLE - 1] += eggs
-        newDay[CHILDBIRTH_CYCLE + CHILDBIRTH_BIRTH_DELAY - 1] += eggs
-        newDay.pop(-1)
-        self.generations.append(newDay)
-        self.fishesByChildbirthDaysLeft = newDay
-
-    def countFishes(self):
-        return sum(self.fishesByChildbirthDaysLeft.values())
-
-
-class Lanternfish(object):
-    def __init__(self, initialValue):
-        self.value = initialValue
-
-    def nextDay(self):
-        self.value -= 1
-        if self.value == -1:
-            self.value = CHILDBIRTH_CYCLE - 1
-            return self.createEgg()
-
-    def createEgg(self):
-        return Lanternfish(CHILDBIRTH_CYCLE + CHILDBIRTH_BIRTH_DELAY - 1)
-
-    def __int__(self):
-        return self.value
-
-    def __str__(self):
-        return str(int(self))
-
-
-def displayFishes(day, fishes):
-    print("After %s day: %s" % (day, ",".join(str(fish) for fish in fishes)))
-
-
-def answer1(numbers, days):
-    lanternfishes = [Lanternfish(number) for number in numbers]
+def answer(numbers, days):
+    fishes = Counter(numbers)
     for day in range(days):
-        eggs = []
-        # displayFishes(day, lanternfishes)
-        for lanternfish in lanternfishes:
-            egg = lanternfish.nextDay()
-            if egg:
-                eggs.append(egg)
-        lanternfishes.extend(eggs)
-        print("%s / %s" % (day + 1, days))
-    # displayFishes(days, lanternfishes)
-    return len(lanternfishes)
+        newFishes = Counter()
+        for dayLeft, amount in fishes.items():
+            newFishes[dayLeft - 1] = amount
+        newFishes[6] += newFishes[-1]
+        newFishes[8] += newFishes[-1]
+        newFishes[-1] = 0
+        fishes = newFishes
+    return sum(fishes.values())
 
-
-def answer2(numbers, days):
-    sea = Sea(numbers)
-    for day in range(days):
-        sea.nextDay()
-        print("%s / %s : %s" % (day + 1, days, sea.countFishes()))
-    return sea.countFishes()
-
-print(answer1(lines, days=80))
-print(answer2(lines, days=256))
+print(answer(lines, 80))
+print(answer(lines, 256))
